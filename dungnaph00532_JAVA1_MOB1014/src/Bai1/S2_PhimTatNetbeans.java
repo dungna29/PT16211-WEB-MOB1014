c6f02c7f9ff17366d4312ce6b853cbb9c0c9adf5
Nguyễn Anh Dũng <55435241+dungna29@users.noreply.github.com>
2020-09-16 15:40:35 +0700

    Dungna29

---
+++ dungnaph00532_JAVA1_MOB1014/src/Bai1/S2_PhimTatNetbeans.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai1;
+
+/**
+ *
+ * @author Nguyen Anh Dung
+ */
+public class S2_PhimTatNetbeans {
+    //Phải gọi hàm main khi mới tạo class
+    //Ctrl + Space(Dấu cách của các bạn)
+    //psvm + tab (Gọi hàm main)
+
+    public static void main(String[] args) {
+        //1. Ctrl + X: delete một dòng. (Hay Dùng)
+        //2. Phím Ctrl + Shift + Up/Down: duplicate một dòng.
+        //3. Phím Ctrl + R: Rename nhiều biến cùng 1 lúc. (Hay Dùng)
+        //4. Phím Ctrl + Shift + O: tìm 1 file bất kỳ trong Project đang mở.
+        //5. Ctrl + Q: trở về vị trí edit cuối cùng.
+        //6. Ctrl + Shift + Down / Up: Duplicate 1 dòng.
+        //7. Ctrl + 7 : Mở nhanh Navigator(kết hợp với phím tab).
+        //8. Ctrl +  / : Bật tắt comment (Hay Dùng)
+        //9. Ctl + B: Di chuyển đến chỗ khai báo function.
+        //10. Shift-F6: chạy file hiện tại (Hay Dùng)
+        //11. Shift-F11: xóa và build project chính
+        //12. Alt + Shift + F: Format lại code cho đàng hoàng (Hay Dùng)
+
+        //Để in ra màn hình abc
+        System.out.println();
+        
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+
+    }
+
+}
